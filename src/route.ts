c527bbcfebe953e4e751ea57acdcd58b674f4b23
Manthan Mallikarjun <nahtnam@gmail.com>
2019-05-06 21:21:25 -0700

    debug

--- src/route.ts
+++ src/route.ts
@@ -19,10 +19,14 @@ type SR = ServerResponse;
 type AP = Promise<any>;
 
 export default (route: Route): Handler => {
+  console.log(1)
   const fn = (Req: IM, Res: SR): AP => {
+    console.log(2)
     let exec = async (req: IM, res: SR): AP => {
+      console.log(3)
       const middleware: any[] = route.middleware || [];
 
+      console.log(4)
       if (fn.log !== false) {
         const pinoOptions = process.env.NODE_ENV === 'production' ? {} : {
           prettyPrint: {
@@ -31,26 +35,36 @@ export default (route: Route): Handler => {
         };
         const logger = pino(pinoOptions);
         middleware.unshift(logger);
+        console.log(5);
       }
 
+      console.log(6)
       for (const mw of middleware) { // eslint-disable-line
         await mw(req, res); // eslint-disable-line
       }
+      console.log(7)
 
       return route.handler(req, res);
     };
+
+    console.log(8)
 
     if (route.plugins) {
       exec = route.plugins.reverse().reduce((acc, val): any => val(acc), exec);
     }
 
+    console.log(9)
+
     const isAWS: boolean = process.env.LIGHT_ENVIRONMENT === 'aws';
     if (isAWS) {
+      console.log(11)
       return AWSServerlessMicro(handleErrors(exec));
     }
+    console.log(10)
     return run(Req, Res, handleErrors(exec));
   };
 
+  console.log(12)
   Object.keys(route).forEach((key): void => {
     (fn as any)[key] = (route as any)[key];
   });
@@ -58,5 +72,6 @@ export default (route: Route): Handler => {
   fn.module = __dirname;
   fn.handler = fn;
 
+  console.log(13)
   return fn;
 };
